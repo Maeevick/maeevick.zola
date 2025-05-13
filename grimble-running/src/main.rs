1062dca050f728f475a68d5568fe08627b835551
Maeevick <maeevick@gmail.com>
2025-05-13 22:13:27 +0200

    feat: basic jump

--- grimble-running/src/main.rs
+++ grimble-running/src/main.rs
@@ -2,26 +2,23 @@ use bevy::prelude::*;
 
 const WINDOW_WIDTH: f32 = 600.0;
 const WINDOW_HEIGHT: f32 = 200.0;
-const GROUND_HEIGHT: f32 = 20.0;
-const GROUND_THICKNESS: f32 = 5.0;
-const GRIMBLE_SIZE: f32 = 50.0;
 const BACKGROUND_COLOR: Color = Color::linear_rgb(1.0, 1.0, 1.0);
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Grimble Running".into(),
-                resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
-                ..default()
-            }),
-            ..default()
-        }))
-        .add_systems(Startup, setup_system)
-        .run();
+const GROUND_HEIGHT: f32 = 20.0;
+const GROUND_THICKNESS: f32 = 5.0;
+
+const GRAVITY: f32 = 980.0;
+
+const GRIMBLE_SIZE: f32 = 50.0;
+const JUMP_FORCE: f32 = 400.0;
+
+#[derive(Component)]
+struct Grimble {
+    velocity: Vec2,
+    on_ground: bool,
 }
 
-fn setup_system(mut commands: Commands, mut clear_color: ResMut<ClearColor>) {
+fn setup(mut commands: Commands, mut clear_color: ResMut<ClearColor>) {
     clear_color.0 = BACKGROUND_COLOR;
     commands.spawn(Camera2d::default());
     commands.spawn((
@@ -46,7 +43,55 @@ fn setup_system(mut commands: Commands, mut clear_color: ResMut<ClearColor>) {
             (-WINDOW_HEIGHT + GRIMBLE_SIZE + GROUND_THICKNESS) / 2.0 + GROUND_HEIGHT,
             1.0,
         ),
+        Grimble {
+            velocity: Vec2::ZERO,
+            on_ground: true,
+        },
     ));
 
     println!("Hello, Grimble! Are you reading for running?");
+}
+
+fn apply_gravity(mut query: Query<&mut Grimble>, time: Res<Time>) {
+    for mut grimble in query.iter_mut() {
+        grimble.velocity.y -= GRAVITY * time.delta_secs();
+    }
+}
+
+fn move_grimble(mut query: Query<(&mut Transform, &mut Grimble)>, time: Res<Time>) {
+    for (mut transform, mut grimble) in query.iter_mut() {
+        transform.translation.y += grimble.velocity.y * time.delta_secs();
+
+        let floor_y = -WINDOW_HEIGHT / 2.0 + GROUND_HEIGHT + GRIMBLE_SIZE / 2.0;
+        if transform.translation.y <= floor_y {
+            transform.translation.y = floor_y;
+            grimble.velocity.y = 0.0;
+            grimble.on_ground = true;
+        }
+    }
+}
+
+fn handle_input(keyboard_input: Res<ButtonInput<KeyCode>>, mut query: Query<&mut Grimble>) {
+    for mut grimble in query.iter_mut() {
+        if keyboard_input.just_pressed(KeyCode::Space) && grimble.on_ground {
+            grimble.velocity.y = JUMP_FORCE;
+            grimble.on_ground = false;
+            println!("Grimble saute!");
+        }
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Grimble is in the Air".into(),
+                resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Startup, setup)
+        .add_systems(Update, (apply_gravity, move_grimble, handle_input))
+        .run();
 }
