ddee893bb381c409f67e6a509d7b8c44dd224acd
Maeevick <maeevick@gmail.com>
2025-05-13 23:11:18 +0200

    feat: game over

--- grimble-running/src/main.rs
+++ grimble-running/src/main.rs
@@ -16,6 +16,12 @@ const GRAVITY: f32 = 980.0;
 const GRIMBLE_SIZE: f32 = 50.0;
 const JUMP_FORCE: f32 = 400.0;
 
+#[derive(Resource)]
+struct GameState {
+    running: bool,
+    score: u32,
+}
+
 #[derive(Component)]
 struct Grimble {
     velocity: Vec2,
@@ -81,7 +87,15 @@ fn move_grimble(mut query: Query<(&mut Transform, &mut Grimble)>, time: Res<Time
     }
 }
 
-fn handle_input(keyboard_input: Res<ButtonInput<KeyCode>>, mut query: Query<&mut Grimble>) {
+fn handle_input(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<&mut Grimble>,
+    game_state: Res<GameState>,
+) {
+    if !game_state.running {
+        return;
+    }
+
     for mut grimble in query.iter_mut() {
         if keyboard_input.just_pressed(KeyCode::Space) && grimble.on_ground {
             grimble.velocity.y = JUMP_FORCE;
@@ -96,7 +110,11 @@ fn move_obstacles(
     mut commands: Commands,
     mut query: Query<(Entity, &mut Transform), With<Obstacle>>,
     time: Res<Time>,
+    game_state: Res<GameState>,
 ) {
+    if !game_state.running {
+        return;
+    }
     for (entity, mut transform) in query.iter_mut() {
         transform.translation.x -= GAME_SPEED * time.delta_secs();
 
@@ -107,7 +125,16 @@ fn move_obstacles(
     }
 }
 
-fn spawn_obstacles(mut commands: Commands, time: Res<Time>, mut timer: ResMut<ObstacleTimer>) {
+fn spawn_obstacles(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut timer: ResMut<ObstacleTimer>,
+    game_state: Res<GameState>,
+) {
+    if !game_state.running {
+        return;
+    }
+
     if timer.0.tick(time.delta()).just_finished() {
         let mut rng = rand::rng();
         let random_width = rng.random_range(0.5..=1.5) * OBSTACLE_SIZE;
@@ -138,6 +165,58 @@ fn spawn_obstacles(mut commands: Commands, time: Res<Time>, mut timer: ResMut<Ob
     }
 }
 
+fn check_collisions(
+    grimble_query: Query<&Transform, With<Grimble>>,
+    obstacle_query: Query<&Transform, With<Obstacle>>,
+    mut game_state: ResMut<GameState>,
+) {
+    if !game_state.running {
+        return;
+    }
+
+    let grimble_transform = match grimble_query.single() {
+        Ok(transform) => transform,
+        Err(_) => {
+            println!("Oho Grimble has disappeared from this world! This is an issue");
+            return;
+        }
+    };
+    let grimble_min_x = grimble_transform.translation.x - GRIMBLE_SIZE / 2.0;
+    let grimble_max_x = grimble_transform.translation.x + GRIMBLE_SIZE / 2.0;
+    let grimble_min_y = grimble_transform.translation.y - GRIMBLE_SIZE / 2.0;
+    let grimble_max_y = grimble_transform.translation.y + GRIMBLE_SIZE / 2.0;
+
+    for obstacle_transform in obstacle_query.iter() {
+        let obstacle_min_x = obstacle_transform.translation.x - OBSTACLE_SIZE / 2.0;
+        let obstacle_max_x = obstacle_transform.translation.x + OBSTACLE_SIZE / 2.0;
+        let obstacle_min_y =
+            obstacle_transform.translation.y - obstacle_transform.scale.y * OBSTACLE_SIZE / 2.0;
+        let obstacle_max_y =
+            obstacle_transform.translation.y + obstacle_transform.scale.y * OBSTACLE_SIZE / 2.0;
+
+        if grimble_max_x > obstacle_min_x
+            && grimble_min_x < obstacle_max_x
+            && grimble_max_y > obstacle_min_y
+            && grimble_min_y < obstacle_max_y
+        {
+            game_state.running = false;
+            println!("You loose! Game Over! You score is: {}", game_state.score);
+        }
+    }
+}
+
+fn update_score(time: Res<Time>, mut game_state: ResMut<GameState>) {
+    if !game_state.running {
+        return;
+    }
+
+    game_state.score += (10.0 * time.delta_secs()) as u32;
+
+    if game_state.score % 10 == 0 && game_state.score > 0 {
+        println!("Score: {}", game_state.score);
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
@@ -152,6 +231,10 @@ fn main() {
             Duration::from_secs(1),
             TimerMode::Repeating,
         )))
+        .insert_resource(GameState {
+            running: true,
+            score: 0,
+        })
         .add_systems(Startup, setup)
         .add_systems(
             Update,
@@ -161,6 +244,8 @@ fn main() {
                 handle_input,
                 spawn_obstacles,
                 move_obstacles,
+                check_collisions,
+                update_score,
             ),
         )
         .run();
