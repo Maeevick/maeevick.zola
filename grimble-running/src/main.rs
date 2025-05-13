62b3eaa69fec6d9f125e9d54cd5156b7e377d5d0
Maeevick <maeevick@gmail.com>
2025-05-13 21:25:41 +0200

    feat: configure camera

--- grimble-running/src/main.rs
+++ grimble-running/src/main.rs
@@ -2,10 +2,20 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-    .add_systems(Update, hello_system)
-    .run();
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Grimble Running".into(),
+                resolution: (800.0, 400.0).into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Startup, setup_system)
+        .run();
 }
 
-fn hello_system() {
-  println!("Hello, Grimble!");
+fn setup_system(mut commands: Commands) {
+    commands.spawn(Camera2d::default());
+
+    println!("Hello, Grimble! Are you reading for running?");
 }
